2aeb0e35f489d1a697f6f5aa56a861c9e7416a79
Lucas Piccolini <lucas.piccolini@gmail.com>
2022-12-06 23:49:10 -0300

    Se mejoro la pestaña Servicios, y se cargaron distintos tipos desde admin

--- ProyectoWeb/urls.py
+++ ProyectoWeb/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ProyectoWebApp/', include('ProyectoWebApp.urls')),
+    path('servicios/', include('Servicios.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -3,7 +3,6 @@ from ProyectoWebApp.views import *
 
 urlpatterns = [
     path('', home, name="Home"),
-    path('servicios/', servicios, name="Servicios"),
     path('tienda/', tienda, name="Tienda"),
     path('blog/', blog, name="Blog"),
     path('contacto/', contacto, name="Contacto"),
